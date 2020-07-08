0d71809d0f2da7ac97ea08a1d0f6d362475db5da
ArgentLo <argent.lo.hk@gmail.com>
2020-07-08 19:32:49 +0900

    smaller model size

--- srnet_model.py
+++ srnet_model.py
@@ -28,13 +28,13 @@ class Srnet_Base(nn.Module):
 			kernel_size=3, stride=1, padding=1, bias=False)
 		self.bn1 = nn.BatchNorm2d(64)
 		# Layer 2
-		self.layer2 = nn.Conv2d(in_channels=64, out_channels=32,
+		self.layer2 = nn.Conv2d(in_channels=64, out_channels=16,
 			kernel_size=3, stride=1, padding=1, bias=False)
-		self.bn2 = nn.BatchNorm2d(32)
+		self.bn2 = nn.BatchNorm2d(16)
 		# Layer 2_Extent
-		self.layer2_ext = nn.Conv2d(in_channels=32, out_channels=16,
-			kernel_size=3, stride=1, padding=1, bias=False)
-		self.bn2_ext = nn.BatchNorm2d(16)
+		# self.layer2_ext = nn.Conv2d(in_channels=32, out_channels=16,
+		# 	kernel_size=3, stride=1, padding=1, bias=False)
+		# self.bn2_ext = nn.BatchNorm2d(16)
 		# Layer 3
 		self.layer31 = nn.Conv2d(in_channels=16, out_channels=16,
 			kernel_size=3, stride=1, padding=1, bias=False)
@@ -129,40 +129,40 @@ class Srnet_Base(nn.Module):
 	def forward(self, inputs):
 		# Layer 1
 		conv = self.layer1(inputs)
-		actv = F.gelu(self.bn1(conv))
+		actv = F.relu(self.bn1(conv))
 		# Layer 2
 		conv = self.layer2(actv)
-		actv = F.gelu(self.bn2(conv))
-		conv = self.layer2_ext(actv)
-		actv = F.gelu(self.bn2_ext(conv))
+		actv = F.relu(self.bn2(conv))
+		# conv = self.layer2_ext(actv)
+		# actv = F.relu(self.bn2_ext(conv))
 		
 		# Layer 3
 		conv1 = self.layer31(actv)
-		actv1 = F.gelu(self.bn31(conv1))
+		actv1 = F.relu(self.bn31(conv1))
 		conv2 = self.layer32(actv1)
 		bn = self.bn32(conv2)
 		res = torch.add(actv, bn)
 		# Layer 4
 		conv1 = self.layer41(res)
-		actv1 = F.gelu(self.bn41(conv1))
+		actv1 = F.relu(self.bn41(conv1))
 		conv2 = self.layer42(actv1)
 		bn = self.bn42(conv2)
 		res = torch.add(res, bn)
 		# Layer 5
 		conv1 = self.layer51(res)
-		actv1 = F.gelu(self.bn51(conv1))
+		actv1 = F.relu(self.bn51(conv1))
 		conv2 = self.layer52(actv1)
 		bn = self.bn52(conv2)
 		res = torch.add(res, bn)
 		# Layer 6
 		conv1 = self.layer61(res)
-		actv1 = F.gelu(self.bn61(conv1))
+		actv1 = F.relu(self.bn61(conv1))
 		conv2 = self.layer62(actv1)
 		bn = self.bn62(conv2)
 		res = torch.add(res, bn)
 		# Layer 7
 		conv1 = self.layer71(res)
-		actv1 = F.gelu(self.bn71(conv1))
+		actv1 = F.relu(self.bn71(conv1))
 		conv2 = self.layer72(actv1)
 		bn = self.bn72(conv2)
 		res = torch.add(res, bn)
@@ -170,7 +170,7 @@ class Srnet_Base(nn.Module):
 		convs = self.layer81(res)
 		convs = self.bn81(convs)
 		conv1 = self.layer82(res)
-		actv1 = F.gelu(self.bn82(conv1))
+		actv1 = F.relu(self.bn82(conv1))
 		conv2 = self.layer83(actv1)
 		bn = self.bn83(conv2)
 		pool = self.pool1(bn)
@@ -179,7 +179,7 @@ class Srnet_Base(nn.Module):
 		convs = self.layer91(res)
 		convs = self.bn91(convs)
 		conv1 = self.layer92(res)
-		actv1 = F.gelu(self.bn92(conv1))
+		actv1 = F.relu(self.bn92(conv1))
 		conv2 = self.layer93(actv1)
 		bn = self.bn93(conv2)
 		pool = self.pool2(bn)
@@ -188,7 +188,7 @@ class Srnet_Base(nn.Module):
 		convs = self.layer101(res)
 		convs = self.bn101(convs)
 		conv1 = self.layer102(res)
-		actv1 = F.gelu(self.bn102(conv1))
+		actv1 = F.relu(self.bn102(conv1))
 		conv2 = self.layer103(actv1)
 		bn = self.bn103(conv2)
 		pool = self.pool1(bn)
@@ -197,14 +197,14 @@ class Srnet_Base(nn.Module):
 		convs = self.layer111(res)
 		convs = self.bn111(convs)
 		conv1 = self.layer112(res)
-		actv1 = F.gelu(self.bn112(conv1))
+		actv1 = F.relu(self.bn112(conv1))
 		conv2 = self.layer113(actv1)
 		bn = self.bn113(conv2)
 		pool = self.pool1(bn)
 		res = torch.add(convs, pool)
 		# Layer 12
 		conv1 = self.layer121(res)
-		actv1 = F.gelu(self.bn121(conv1))
+		actv1 = F.relu(self.bn121(conv1))
 		conv2 = self.layer122(actv1)
 		bn = self.bn122(conv2)
 		# print("L12:",res.shape)
